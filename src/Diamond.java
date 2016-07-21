f935841ffc97d76f2e3afce6f34a208d05e3b0e2
XiaoVLiu <xwliu@thoughtworks.com>
2016-07-21 13:52:51 +0800

    rename drawIsoscelesDiamond to drawIsoscelesTriangle

--- src/Diamond.java
+++ src/Diamond.java
@@ -4,10 +4,10 @@ public class Diamond {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Diamond.drawIsoscelesDiamond(Integer.parseInt(scanner.nextLine()));
+        Diamond.drawIsoscelesTriangle(Integer.parseInt(scanner.nextLine()));
     }
 
-    private static void drawIsoscelesDiamond(int number) {
+    private static void drawIsoscelesTriangle(int number) {
         for (int i=0; i<number; i++) {
             for (int j=0; j<number-i-1; j++) {
                 System.out.print(" ");
